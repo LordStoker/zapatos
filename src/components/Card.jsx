555d5bc2d5dbb89defca990123afc6608444d9d2
LordStoker <tpc00@iesemilidarder.com>
2024-10-22 19:32:16 +0200

    commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,33 @@
+import {data} from './../data/data.js';
+
+
+export default function Card(){
+    return(    
+        <>
+            {data.map((product) => (
+                <article className='card'>
+                
+                <section key={product.id} className="card-container-img">
+                    {product.newCollection && 
+                    <span className='new'><img className = "star" src= {`/images/${product.imgs.imgStar}`} alt="estrella" /></span>}
+                    {product.stock > 45 ? <img className = "img-product" src= {`/images/${product.imgs.imgProduct}`} alt="product" />
+                     
+                    :<img className = "img-product" src= {`/images/${product.imgs.imgSoldOut}`} alt="product" />} 
+                </section>
+                    <section>
+                        <h2>{product.name}</h2> 
+                        <h2>{product.description}</h2>
+                        <h2>{product.price}€ {product.price <200 && <span style={{color:"red", marginLeft: "10px"}}>30% off</span>}</h2> 
+                </section> 
+                    
+               
+               </article> 
+                
+            ))}
+            </>
+        
+        
+    );
+    
+    
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header(){
+    return(
+    <div className="header-container">
+        <img src= "/images/rocket.gif" alt="rocket" />
+        <h2>
+            Rocket store
+        </h2>
+    </div>
+    );
+}
